3db45bc853094c8f50d4a0ac79c6751060bca9d9
kossaych <cheminguikossay@gmail.com>
2024-01-04 18:57:32 +0100

    add requirements.txt

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--uw560b&8i8_w3xyp36mvuanam-nnbst!9wbdko*#$hmt2ta+a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.111','127.0.0.1','localhost','.vercel.app']
+ALLOWED_HOSTS = ['192.168.1.111','127.0.0.1','localhost']#,'.vercel.app']
 
 
 # Application definition
