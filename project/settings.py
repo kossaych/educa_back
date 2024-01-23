ce0f2a9a31dfb15a4c62799f0a43b3dd53073fbf
kossaych <cheminguikossay@gmail.com>
2024-01-23 17:16:32 +0100

    deploy

--- project/settings.py
+++ project/settings.py
@@ -169,13 +169,16 @@ STATIC_URL = 'static/'
 
 import os
  
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media' )
-    
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ 
+
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
