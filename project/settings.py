590600fd226e1fd005b8f92b91459e6f335fe6b6
kossaych <cheminguikossay@gmail.com>
2024-01-23 14:03:26 +0100

    deploy

--- project/settings.py
+++ project/settings.py
@@ -178,16 +178,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
  
+
+
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-
-
+EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_HOST_USER = 'cheminguikossay@gmail.com'
-EMAIL_HOST_PASSWORD = 'odeckaxrrnulnxfd'
+EMAIL_HOST_PASSWORD = 'iqqqolufqmoxpfmp' 
+EMAIL_PORT = 587 
 
 
-EMAIL_PORT = 587
 
- 
+
+
 AUTH_USER_MODEL = 'content.BaseUser'
