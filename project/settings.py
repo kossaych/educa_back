5240947cffc60587da67e3b22c6057e5098572e2
kossaych <cheminguikossay@gmail.com>
2024-01-31 18:49:52 +0100

    deployment

--- project/settings.py
+++ project/settings.py
@@ -98,16 +98,16 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-""" 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-} """
+}
 
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'verceldb',
@@ -117,7 +117,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+ """
 
 
 """DATABASES = {
