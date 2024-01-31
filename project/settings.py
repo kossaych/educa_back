e13b856b884b6c0d523cc27e562c864889d3cf73
kossaych <cheminguikossay@gmail.com>
2024-01-31 19:49:06 +0100

    deployment

--- project/settings.py
+++ project/settings.py
@@ -68,8 +68,15 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/' 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
+
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
@@ -99,15 +106,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} """
 
 
-""" DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'verceldb',
@@ -117,7 +124,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
- """
+
 
 
 """DATABASES = {
@@ -161,14 +168,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 
-import os
- 
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
  
